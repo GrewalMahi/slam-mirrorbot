891c746a7f97f622c96dd882ecdd9b563ba77de6
GrewalMahi <75774204+GrewalMahi@users.noreply.github.com>
2021-05-19 06:23:08 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,27 +1,8 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start2'
-        self.MirrorCommand = 'mirror2'
-        self.UnzipMirrorCommand = 'unzipmirror2'
-        self.TarMirrorCommand = 'tarmirror2'
-        self.CancelMirror = 'cancel2'
-        self.CancelAllCommand = 'cancelall2'
-        self.ListCommand = 'list2'
-        self.StatusCommand = 'status2'
-        self.AuthorizeCommand = 'authorize2'
-        self.UnAuthorizeCommand = 'unauthorize2'
-        self.AuthListCommand = 'authlist2'
-        self.PingCommand = 'ping2'
-        self.RestartCommand = 'restart2'
-        self.StatsCommand = 'stats2'
-        self.HelpCommand = 'help2'
-        self.LogCommand = 'log2'
-        self.SpeedCommand = 'speedtest2'
-        self.CloneCommand = 'clone2'
-        self.WatchCommand = 'watch'
-        self.TarWatchCommand = 'tarwatch2'
-        self.DeleteCommand = 'del2'
-        self.UsageCommand = 'usage2'
-        self.RepoCommand = 'repo2'
-
+        self.MirrorCommand = 'mirror2'        
+        self.CancelMirror = 'cancel2'        
+        self.StatusCommand = 'status2'      
+        
 BotCommands = _BotCommands()
